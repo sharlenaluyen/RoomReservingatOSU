1ac05b4f8b3575420b32df0f6e622d1420ac3042
boxman888 <epatterson888@hotmail.com>
2017-11-16 12:48:23 -0800

    connectvarsEECS is used to hold DB admin login creds

---
+++ connectvarsEECS.php
@@ -0,0 +1,8 @@
+<?php
+  // Define database connection constants
+  //define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_HOST', 'access.engr.oregonstate.edu');
+  define('DB_USER', '[Add user name');
+  define('DB_PASSWORD', '[add password]');
+  define('DB_NAME', '[add db name]');
+?>
