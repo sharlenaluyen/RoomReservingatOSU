e0f3271d35abddaeef90e30f5bef2f4bbe2458c2
boxman888 <epatterson888@hotmail.com>
2017-11-23 20:22:58 -0800

    Added a user login.

--- loginStuff/signUp.php
+++ loginStuff/signUp.php
@@ -1,6 +1,7 @@
 <html>
   <head>
 	<link rel="stylesheet" href="css/master.css"/>
+        <link rel="stylesheet" href="css/index.css"/>
   </head>
   <body>
     <div id="signUp">
@@ -12,13 +13,50 @@
 	<input type="password" for="sPassword" name="sPassword"id="sPassword" placeholder="Enter Password"/>
 	<input type="submit" id="signUp-btn" value="Sign Up">
       </form>
-      <form>
-        <h1>Already a user?</h1>
-        <input type="button" id="signUp-btn" value="Login"/>
-      </form>
-      <form action="../index.html" method="get">
-        <input type="submit" value="Back">
-      </form>
+      <div>
+	 <h1>Already a user?</h1>
+        <button onclick="document.getElementById('id01').style.display='block'">Login</button>
+      </div>
+        <form action="../index.html" method="get">
+          <input type="submit" value="Back">
+        </form>
     </div>
+    </div>
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="images/img_avatar.png" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+      <input type="checkbox" checked="checked"> Remember me
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
   </body>
 </html>
