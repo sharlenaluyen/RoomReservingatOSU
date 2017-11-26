1b3217351bf129ed8a3d635a8996dc12bcbe2922
boxman888 <epatterson888@hotmail.com>
2017-11-25 17:34:19 -0800

    Added some stuff.

---
+++ aboutUs/FAQ/faq.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+  </head>
+  <body>
+    <div class="info-box">
+       <p>
+	Students have massive difficulty searching for study rooms on campus to use for personal use, group use, and/or practice a presentation.
+	Examples of high-priority rooms include those with projectors, HDMI hook up, whiteboards, quiet areas, and outlets -- just to name a few.
+	Our application would like to target students with specific needs to observe which rooms have available equipment, 
+	are empty, and provide the most optimal study space for their needs.
+       <p/>
+       <div class="icons">
+        <a href="../../aboutUs.php" class="button">Back</a>
+       </div>
+    </div>
+  </body>
+</html>
