304e15c8867a8d9d945ef33a4368a67e486a0436
boxman888 <epatterson888@hotmail.com>
2017-11-28 19:10:19 -0800

    added an account page and made a few edits to a users session.

--- loginStuff/action_page.php
+++ loginStuff/action_page.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html
-
-<!-- Insert into Student table CS 340 -->
-<html>
-	<head>
-		<title>Student Table Viewer</title>
-		<link rel="stylesheet" href="index.css">
-		<style>
-			html{
-  				background-image:url(images/justinBissonBeck.jpg);
-  				background-repeat:no-repeat;
-  				background-size:contain;
-  				-webkit-background-size:cover;
-  				-moz-background-size:cover;
-  				-o-background-size:cover;
- 				 background-size:cover;
- 				 font-family:'Helvetica Neue', sans-serif;	
-			}
-		</style>
-	</head>
-<body>
 <?php
 	session_start();
 // change the value of $dbuser and $dbpass to your username and password
@@ -36,23 +15,21 @@
 // qurery = looks for a sUsername in the Users table 
 	$query = "SELECT * FROM `Project_Users` WHERE Username = '$user'";// 'useris unique
 	$result = mysqli_query($conn, $query);// Return the results of the crery
-	// Look at individual cell data. Should check if query faild, howev cin this 
+	// Look at individual cell da	ta. Should check if query faild, howev cin this 
 	// instance it will not fail. data is eather there or not.c
 
 	$row = mysqli_fetch_assoc($result);// Brakes the qurery into one ro
 					// Lets use get access to each cell in the row
 	$salt = $row["salt"];// Get data from the salt column
 	$dbPass = $row["Password"];// Get data from the sPassword column
+	$ONID = $row["OSU_ID"];
 	
 	$pass = md5($pass . $salt);
 	// If statment checks if hashed password is the same password in the database
 	if ($pass == $dbPass){
-		echo "<p>User loged In</p>";
-		echo "<form action='../index.php' method='post'>";
-		echo "<input type='submit' value='Home'>";
-		$_SESSION["usr"] = $user; 
-		echo "<a>Loged in as " . $_SESSION["usr"] . "</a>";
-		echo print_r($_SESSION);
+		$_SESSION["usr"] = $user;
+		$_SESSION["onid"] = $ONID;
+		header("Location: ../index.php");
 	}
 	else{
 		echo "Username/Password not found";
