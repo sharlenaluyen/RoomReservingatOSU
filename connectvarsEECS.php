0112e9196aba5c6bf608c62a490a6314993a2c42
Shanmukh Challa <challas@flip3.engr.oregonstate.edu>
2017-12-03 20:46:14 -0800

    Added styling changes to reserve

--- connectvarsEECS.php
+++ connectvarsEECS.php
@@ -1,7 +1,7 @@
 <?php
   // Define database connection constants
   //define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_HOST', 'access.engr.oregonstate.edu');
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
   define('DB_USER', 'cs340_luyens');
   define('DB_PASSWORD', '3370');
   define('DB_NAME', 'cs340_luyens');
